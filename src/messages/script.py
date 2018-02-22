22d20ad5f1f7f8532f79e67dcc2e3546617e46a4
Bartłomiej Górkiewicz <bartlomiej.gorkiewicz@gmail.com>
2018-02-22 18:18:22 +0100

    fixing bugs

--- src/messages/script.py
+++ src/messages/script.py
@@ -7,8 +7,10 @@ import random
 
 
 def monthToNum(month):
-	json_data = json.load(open('months.json'))
-	return json_data[monthLang][str(month)]
+	json_data = json.load(open('months.json', encoding='utf8'))
+	for x in json_data[monthLang]:
+		if str(x) == str(month):
+			return json_data[monthLang][x]
 
 def fbDateToWeekday(date1):
 	date1 = date1.split()
@@ -47,7 +49,7 @@ index.write('<!DOCTYPE html><html><head><meta charset="UTF-8" /><meta http-equiv
 
 members = {}
 dates = {}
-hours = {'0': 0, '1': 0, '2': 0,'3': 0, '4': 0, '5': 0,'6': 0, '7': 0, '8': 0,'9': 0, '10': 0, '11': 0,'12': 0, '13': 0, '14': 0,'15': 0, '16': 0, '17': 0,'18': 0, '19': 0, '20': 0,'21': 0, '22': 0, '23': 0}
+hours = {'00': 0, '01': 0, '02': 0,'03': 0, '04': 0, '05': 0,'06': 0, '07': 0, '08': 0,'09': 0, '10': 0, '11': 0,'12': 0, '13': 0, '14': 0,'15': 0, '16': 0, '17': 0,'18': 0, '19': 0, '20': 0,'21': 0, '22': 0, '23': 0}
 weekDays = {'Monday': 0, 'Tuesday': 0, 'Wednesday': 0, 'Thursday': 0, 'Friday': 0, 'Saturday': 0, 'Sunday': 0}
 allWords = {}
 startDate = ""
@@ -62,7 +64,7 @@ messagesPerDay = 0
 pAmount = 0
 
 print("Reading messages...    ", end="")
-for w in range(messagesAmount-1, 0, -1):
+for w in range(messagesAmount-1, -1, -1):
 	user = messages[w].find('span', attrs={'class': 'user'})
 	date = messages[w].find('span', attrs={'class': 'meta'})
 	message = p[pAmount]
@@ -85,12 +87,13 @@ for w in range(messagesAmount-1, 0, -1):
 	totalWords += len(words)
 
 	for word in words:
+		word = word.lower()
 		if word in allWords:
 			allWords[word] += 1
 		else:
 			allWords[word] = 1
 
-	if str(hour) in hours:
+	if hour in hours.keys():
 		hours[hour] += 1
 	else:
 		hours[hour] = 1
